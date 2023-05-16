1d534d2c9fcf6059667961c11d138e1bd90fe95e
Johannes Krabbe <Johannes@Krabbe.net>
2023-05-16 23:17:48 +0200

    token fix

--- src/hooks/token.ts
+++ src/hooks/token.ts
@@ -3,20 +3,14 @@ import { useEffect, useState } from "react";
 export const useToken = () => {
   const tokenName = "donkey-token";
 
-  const getToken = (): string => {
-    let token;
-    useEffect(() => {
-      token = localStorage.getItem(tokenName);
-    });
-    return token ? token : "";
-  };
+  const [token, setToken] = useState<string | null>(null);
 
-  const [token, setToken] = useState(getToken());
+  useEffect(() => {
+    setToken(localStorage.getItem(tokenName));
+  }, []);
 
   const saveToken = (token: string) => {
     localStorage.setItem(tokenName, token);
-
-    setToken(token);
   };
 
   return {
