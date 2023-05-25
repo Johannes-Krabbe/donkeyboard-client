a0c8b8f0a9b93a98a326de4523692e4f376c84b5
Johannes Krabbe <Johannes@Krabbe.net>
2023-05-25 17:12:52 +0200

    fix chakraUI integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { Rubik } from "next/font/google";
 
 const rubik = Rubik({ subsets: ["latin"] });
@@ -9,33 +7,14 @@ export const metadata = {
   description: "Test your type speed",
 };
 
-export const theme = extendTheme({
-  fonts: {
-    heading: "var(--font-rubik)",
-    body: "var(--font-rubik)",
-  },
-});
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <style jsx global>
-        {`
-          :root {
-            --font-rubik: ${rubik.style.fontFamily};
-          }
-        `}
-      </style>
-
-      <html lang="en">
-        <body className={rubik.className}>
-          <ChakraProvider>{children}</ChakraProvider>
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={rubik.className}>{children}</body>
+    </html>
   );
 }
