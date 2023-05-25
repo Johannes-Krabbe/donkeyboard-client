e3f9262fa6f4c3c5b18e1038a174840e9c04d1b6
Johannes Krabbe <Johannes@Krabbe.net>
2023-05-25 18:26:20 +0200

    Implement timer and wpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,11 @@ export default function Home() {
   const [wordcount, setwordcount] = useState(3);
   const [userInput, setUserInput] = useState("");
   const [wordList, setWordList] = useState([""]);
+  const [timer, setTimer] = useState(0);
+  const [referenceTime, setReferenceTime] = useState(Date.now());
+  const [timerActive, setTimerActive] = useState(false);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
 
   const handleKeyDown = (event: any) => {
     if (!document.hasFocus()) {
@@ -21,14 +26,18 @@ export default function Home() {
       reset();
     } else if (event.key.length === 1) {
       // This check is to ensure that only printable characters are added
-      if (!(event.key === " " && userInput.at(-1) == " "))
+      if (!(event.key === " " && userInput.at(-1) == " ")) {
         setUserInput(userInput + event.key);
+        setTimerActive(true);
+      }
     }
   };
 
   const reset = () => {
     setWordList(randomWords(wordcount));
     setUserInput("");
+    setTimer(0);
+    setTimerActive(false);
   };
 
   useEffect(() => {
@@ -48,6 +57,34 @@ export default function Home() {
       reset();
     }
   });
+
+  useEffect(() => {
+    if (userInput != "" && userInput === wordList.join(" ")) {
+      setTimerActive(false);
+
+      // calc wpm
+      const time = timer / 1000 / 60;
+      console.log(time);
+      setCurrentScore(wordcount / time);
+      if (currentScore > highscore) {
+        setHighscore(currentScore);
+      }
+    }
+  }, [userInput, wordList, currentScore, highscore, timer, wordcount]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (timerActive) {
+        setTimer((prevTime) => {
+          const now = Date.now();
+          const interval = now - referenceTime;
+          setReferenceTime(now);
+          return prevTime + interval;
+        });
+      }
+    });
+    return () => clearInterval(interval);
+  }, [timer, timerActive, referenceTime]);
 
   return (
     <main>
@@ -102,6 +139,9 @@ export default function Home() {
           })}
         </h1>
       </div>
+      <h2>Time: {(timer / 1000).toFixed(3)}</h2>
+      <h2>Current Score: {currentScore.toFixed(1)} WPM</h2>
+      <h2>Highscore: {highscore.toFixed(1)} WPM</h2>
       <h2>guide:</h2>
       <ul>
         <li>start typing</li>
